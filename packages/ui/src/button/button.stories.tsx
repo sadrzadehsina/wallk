7b9213fa6f0ddf522b05c2e4224a1ef22f7d60ae
Sina Sadrzadeh <sadrzadeh.sinaa@gmail.com>
2023-03-03 08:05:28 +0330

    Add a sample story for button

---
+++ packages/ui/src/button/button.stories.tsx
@@ -0,0 +1,20 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Button } from './button';
+
+export default {
+  title: 'Components/Button',
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+  label: 'Button',
+};
+
