218b78b11f776acc112d648444b95d09b2a47317
Sina Sadrzadeh <sadrzadeh.sinaa@gmail.com>
2023-03-03 07:57:02 +0330

    Add storybook to packages/ui

---
+++ packages/ui/.storybook/main.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+	core: {
+		builder: "webpack5",
+	},
+	stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+	addons: [
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@storybook/addon-interactions",
+		{
+			name: "@storybook/addon-postcss",
+			options: {
+				cssLoaderOptions: {
+					importLoaders: 1,
+				},
+				postcssLoaderOptions: {
+					implementation: require("postcss"),
+				},
+			},
+		},
+	],
+	framework: "@storybook/react",
+	webpackFinal: async (config) => {
+		config.module.rules.push({
+			test: /\.css$/i,
+			use: [
+				{
+					loader: "postcss-loader",
+					options: { implementation: require.resolve("postcss") },
+				},
+			],
+			include: path.resolve(__dirname, "../"),
+		});
+		// Return the altered config
+		return config;
+	}
+}
