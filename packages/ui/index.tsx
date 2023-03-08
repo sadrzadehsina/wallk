b88bd299c5e430f095423781b3aa9d085792bbd7
Sina Sadrzadeh <sadrzadeh.sinaa@gmail.com>
2023-03-08 12:45:18 +0330

    Update base input to work as a child of controlled input

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,3 +1,5 @@
 export * from "./src/button";
 export * from "./src/header";
-export * from './src/steps';
+export * from "./src/steps";
+export * from "./src/input";
+export * from "./src/select";
--- packages/ui/src/input/input.tsx
+++ packages/ui/src/input/input.tsx
@@ -1,12 +1,16 @@
+import { ChangeEvent } from "react";
+
 type InputProps = {
 	label?: string;
 	required?: boolean;
 	placeholder?: string;
 	error?: string;
+	onChange?: (event: ChangeEvent) => void;
+	name: string;
 };
 
 export function Input(props: InputProps) {
-	const { label, required, placeholder, error } = props;
+	const { label, required, placeholder, error, onChange, name } = props;
 
 	return (
 		<div className="form-control w-full max-w-xs">
@@ -16,11 +20,13 @@ export function Input(props: InputProps) {
 				</span>
 			</label>
 			<input
+				name={name}
 				type="text"
 				placeholder={placeholder}
 				className={`input-bordered input w-full max-w-xs ${
 					error ? "input-error" : ""
 				}`}
+				onChange={onChange}
 			/>
 			<label className="label">
 				<span className="label-text-alt text-error">{error}</span>
