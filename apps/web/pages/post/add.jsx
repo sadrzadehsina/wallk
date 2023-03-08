2f45478b50827408c02e6c8da00b9488ae4cfbad
Sina Sadrzadeh <sadrzadeh.sinaa@gmail.com>
2023-03-08 12:46:02 +0330

    Use controlled input in post add page

--- apps/web/pages/post/add.jsx
+++ apps/web/pages/post/add.jsx
@@ -1,4 +1,6 @@
 import { Stepper } from "ui";
+import { FormikProps, Form, Formik } from "formik";
+import { ControlledInput } from "../../components/form-inputs/controlled-input";
 
 export default function PostAdd() {
 	return (
@@ -9,7 +11,20 @@ export default function PostAdd() {
 				<Stepper.Button value={3}>Step 3</Stepper.Button>
 			</Stepper.Buttons>
 			<Stepper.Contents>
-				<Stepper.Content value={1}>Step 1 Content</Stepper.Content>
+				<Stepper.Content value={1}>
+					<Formik initialValues={{ title: "" }}>
+						{(props) => (
+							<Form>
+								<ControlledInput
+									name="title"
+									label="Title"
+									required
+									placeholder="Type post title"
+								/>
+							</Form>
+						)}
+					</Formik>
+				</Stepper.Content>
 				<Stepper.Content value={2}>Step 2 Content</Stepper.Content>
 				<Stepper.Content value={3}>Step 3 Content</Stepper.Content>
 			</Stepper.Contents>
