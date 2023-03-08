2f87a840557373acd6281c2074def4a8f878fb0a
Sina Sadrzadeh <sadrzadeh.sinaa@gmail.com>
2023-03-08 08:55:24 +0330

    Add namespace for nested stepper components

--- packages/ui/src/steps/index.ts
+++ packages/ui/src/steps/index.ts
@@ -1,1 +1,15 @@
-export * from './steps';
+import {
+	Stepper as StepperContainer,
+	StepButtons,
+	StepButton,
+	StepContents,
+	StepContent,
+} from "./steps";
+
+export const Stepper = {
+	Container: StepperContainer,
+	Buttons: StepButtons,
+	Button: StepButton,
+	Contents: StepContents,
+	Content: StepContent,
+};
--- packages/ui/src/steps/steps.stories.tsx
+++ packages/ui/src/steps/steps.stories.tsx
@@ -1,34 +1,28 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import {
-	Stepper,
-	StepButtons,
-	StepButton,
-	StepContents,
-	StepContent,
-} from "./steps";
+import { Stepper } from ".";
 
 export default {
 	title: "Components/Steps",
-	component: Stepper,
-} as ComponentMeta<typeof Stepper>;
+	component: Stepper.Container,
+} as ComponentMeta<typeof Stepper.Container>;
 
-const Template: ComponentStory<typeof Stepper> = (args) => {
+const Template: ComponentStory<typeof Stepper.Container> = (args) => {
 	return (
-		<Stepper {...args}>
-			<StepButtons>
-				<StepButton value={1}>Step1</StepButton>
-				<StepButton value={2}>Step2</StepButton>
-				<StepButton value={3}>Step3</StepButton>
-				<StepButton value={4}>Step4</StepButton>
-			</StepButtons>
-			<StepContents>
-				<StepContent value={1}>Step1 Content</StepContent>
-				<StepContent value={2}>Step2 Content</StepContent>
-				<StepContent value={3}>Step3 Content</StepContent>
-				<StepContent value={4}>Step4 Content</StepContent>
-			</StepContents>
-		</Stepper>
+		<Stepper.Container {...args}>
+			<Stepper.Buttons>
+				<Stepper.Button value={1}>Step1</Stepper.Button>
+				<Stepper.Button value={2}>Step2</Stepper.Button>
+				<Stepper.Button value={3}>Step3</Stepper.Button>
+				<Stepper.Button value={4}>Step4</Stepper.Button>
+			</Stepper.Buttons>
+			<Stepper.Contents>
+				<Stepper.Content value={1}>Step1 Content</Stepper.Content>
+				<Stepper.Content value={2}>Step2 Content</Stepper.Content>
+				<Stepper.Content value={3}>Step3 Content</Stepper.Content>
+				<Stepper.Content value={4}>Step4 Content</Stepper.Content>
+			</Stepper.Contents>
+		</Stepper.Container>
 	);
 };
 
