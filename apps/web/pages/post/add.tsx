47ae799c2e22a6a9fc053664528534c333adfbf7
Sina Sadrzadeh <sadrzadeh.sinaa@gmail.com>
2023-03-10 12:04:51 +0330

    Update add post page with wizard component

--- apps/web/pages/post/add.tsx
+++ apps/web/pages/post/add.tsx
@@ -4,44 +4,48 @@ import { ControlledInput } from "../../components/form-inputs/controlled-input";
 import { ControlledSelect } from "../../components/form-inputs/controlled-select";
 import { ControlledTextArea } from "../../components/form-inputs/controlled-text-area";
 
+import { Wizard, WizardStep } from "../../components/wizard";
+
 export default function PostAdd() {
 	return (
-		<Stepper.Container active={1}>
-			<Stepper.Buttons>
-				<Stepper.Button value={1}>Step 1</Stepper.Button>
-				<Stepper.Button value={2}>Step 2</Stepper.Button>
-				<Stepper.Button value={3}>Step 3</Stepper.Button>
-			</Stepper.Buttons>
-			<Stepper.Contents>
-				<Stepper.Content value={1}>
-					<Formik initialValues={{ title: "" }}>
-						{(props) => (
-							<Form>
-								<div className="grid grid-cols-2 gap-x-20 gap-y-5">
-									<ControlledInput
-										name="title"
-										label="Title"
-										required
-										placeholder="Type post title"
-									/>
-									<ControlledSelect name="category" label="Category" required>
-										<Select.Option>Category 1</Select.Option>
-										<Select.Option>Category 2</Select.Option>
-										<Select.Option>Category 3</Select.Option>
-									</ControlledSelect>
-									<ControlledTextArea
-										name="short-decription"
-										label="Short Description"
-									/>
-									<ControlledTextArea name="decription" label="Description" />
-								</div>
-							</Form>
-						)}
-					</Formik>
-				</Stepper.Content>
-				<Stepper.Content value={2}>Step 2 Content</Stepper.Content>
-				<Stepper.Content value={3}>Step 3 Content</Stepper.Content>
-			</Stepper.Contents>
-		</Stepper.Container>
+		<Wizard
+			initialValues={{
+				title: "",
+				category: "",
+				shortDecription: "",
+				description: "",
+			}}
+		>
+			<WizardStep>
+				<div className="grid grid-cols-2 gap-x-20 gap-y-5">
+					<ControlledInput
+						name="title"
+						label="Title"
+						required
+						placeholder="Type post title"
+					/>
+					<ControlledSelect name="category" label="Category" required>
+						<Select.Option>Category 1</Select.Option>
+						<Select.Option>Category 2</Select.Option>
+						<Select.Option>Category 3</Select.Option>
+					</ControlledSelect>
+					<ControlledTextArea
+						name="shortDecription"
+						label="Short Description"
+					/>
+					<ControlledTextArea name="decription" label="Description" />
+				</div>
+			</WizardStep>
+			<WizardStep>
+				<div className="grid grid-cols-2 gap-x-20 gap-y-5">
+					<ControlledInput
+						name="title"
+						label="Title"
+						required
+						placeholder="Type post title"
+					/>
+				</div>
+			</WizardStep>
+		</Wizard>
 	);
 }
